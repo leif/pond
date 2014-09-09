a34813875dae91de3501ee38e914fbc3c872e819
Leif Ryge <leif@synthesize.us>
2014-09-09 14:54:52 +0000

    show empty (ack) messages in outbox
    so they can be deleted when DELIVERY_SIGNATURE_INVALID happens

--- client/cli.go
+++ client/cli.go
@@ -891,9 +891,11 @@ func (c *cliClient) outboxSummary() (table cliTable) {
 			continue
 		}
 
+/*
 		if len(msg.message.Body) == 0 {
 			continue
 		}
+*/
 
 		if msg.cliId == invalidCliId {
 			msg.cliId = c.newCliId()
